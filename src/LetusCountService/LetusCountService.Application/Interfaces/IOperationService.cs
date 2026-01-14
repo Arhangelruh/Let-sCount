e0db050535be23fb9da8727923e73205a145a741
Ruh <ruh@tut.by>
2026-01-14 09:28:21 +0300

    init: added interfaces and service to work with EF

---
+++ src/LetusCountService/LetusCountService.Application/Interfaces/IOperationService.cs
@@ -0,0 +1,14 @@
+﻿using LetusCountService.Domain.Models;
+
+namespace LetusCountService.Application.Interfaces
+{
+	public interface IOperationService
+	{
+		/// <summary>
+		/// Add operation.
+		/// </summary>
+		/// <param name="operation">Operation model</param>
+		/// <returns></returns>
+		Task<int> AddOperationAsync(Operation operation);
+	}
+}
---
+++ src/LetusCountService/LetusCountService.Application/Interfaces/IOperationUnitService.cs
@@ -0,0 +1,14 @@
+﻿using LetusCountService.Domain.Models;
+
+namespace LetusCountService.Application.Interfaces
+{
+	public interface IOperationUnitService
+	{
+		/// <summary>
+		/// Add operation unit.
+		/// </summary>
+		/// <param name="operationUnit">OperationUnit model</param>
+		/// <returns></returns>
+		Task<int> AddOperationUnitAsync(OperationUnit operationUnit);
+	}
+}
