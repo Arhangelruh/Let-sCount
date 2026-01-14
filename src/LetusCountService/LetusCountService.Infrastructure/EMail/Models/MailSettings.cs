e2c46ea23d0274020c025d891a4a6017ee4bae60
Ruh <ruh@tut.by>
2026-01-14 09:32:05 +0300

    init: added logic to send email

---
+++ src/LetusCountService/LetusCountService.Infrastructure/EMail/Models/MailSettings.cs
@@ -0,0 +1,35 @@
+﻿namespace LetusCountService.Infrastructure.EMail.Models
+{
+	public class MailSettings
+	{
+		/// <summary>
+		/// Server addres.
+		/// </summary>
+		public string? SMTPServer { get; set; }
+
+		/// <summary>
+		/// Server port.
+		/// </summary>
+		public string? SMTPPort { get; set; }
+
+		/// <summary>
+		/// Login for SMTP.
+		/// </summary>
+		public string? SMTPLogin { get; set; }
+
+		/// <summary>
+		/// Password for SMTP.
+		/// </summary>
+		public string? SMTPPassword { get; set; }
+
+		/// <summary>
+		/// List of receivers.
+		/// </summary>
+		public string? Sender { get; set; }
+
+		/// <summary>
+		/// List of receivers.
+		/// </summary>
+		public List<string>? Receivers { get; set; }
+	}
+}
