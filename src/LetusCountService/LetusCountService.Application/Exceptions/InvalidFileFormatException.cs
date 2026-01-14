9a9c451f9af59f28569e8f1c3462e0c021eeb7c9
Ruh <ruh@tut.by>
2026-01-14 09:34:01 +0300

    init: added custom exceptions for app layer.

---
+++ src/LetusCountService/LetusCountService.Application/Exceptions/InvalidFileFormatException.cs
@@ -0,0 +1,17 @@
+﻿namespace LetusCountService.Application.Exceptions
+{
+	/// <summary>
+	/// File format exception.
+	/// </summary>
+	/// <param name="filePath">Path to file</param>
+	/// <param name="innerException">Exception</param>
+	public sealed class InvalidFileFormatException(
+		string filePath,
+		Exception innerException)
+		: FileProcessingException(
+			filePath,
+			$"Incorrect file {filePath} format",
+			innerException)
+	{
+	}
+}
