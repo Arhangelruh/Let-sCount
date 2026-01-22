0ff33b362e33c3ad6ca3c6275c9e631c6876c891
Ruh <ruh@tut.by>
2026-01-22 14:53:01 +0300

    init: add ef core models and comon constants to domain layer.

---
+++ src/LetusCountApplication/LetusCountApplication.Domain/Models/EFModels/Cash.cs
@@ -0,0 +1,30 @@
+﻿namespace LetusCountApplication.Domain.Models.EFModels
+{
+	public class Cash
+	{
+		/// <summary>
+		/// Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Cash desk name.
+		/// </summary>
+		public required string Name { get; set; }
+
+		/// <summary>
+		/// Kisan number.
+		/// </summary>
+		public required string Kisan { get; set; }
+
+		/// <summary>
+		/// Department identifier.
+		/// </summary>
+		public int DepartmentId { get; set; }
+
+		/// <summary>
+		/// Navigate to Operation.
+		/// </summary>
+		public Department Department { get; set; }
+	}
+}
---
+++ src/LetusCountApplication/LetusCountApplication.Domain/Models/EFModels/Department.cs
@@ -0,0 +1,25 @@
+﻿namespace LetusCountApplication.Domain.Models.EFModels
+{
+	public class Department
+	{
+		/// <summary>
+		/// Banknote id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Department name.
+		/// </summary>
+		public required string Name { get; set; }
+
+		/// <summary>
+		/// Department address.
+		/// </summary>
+		public string? Address { get; set; }
+
+		/// <summary>
+		///  Navigate to Operation units.
+		/// </summary>
+		public ICollection<Cash> Cashes { get; set; }
+	}
+}
