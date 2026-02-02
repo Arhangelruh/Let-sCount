c15d2d0101313c5b37fc336d77daa8f263d0296e
Ruh <ruh@tut.by>
2026-02-02 09:11:03 +0300

    init: add domain models

---
+++ src/LetusCountApplication/LetusCountApplication.Domain/Models/Cash.cs
@@ -0,0 +1,30 @@
+﻿namespace LetusCountApplication.Domain.Models
+{
+	public class Cash
+	{
+		/// <summary>
+		/// Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Cash desk name.
+		/// </summary>
+		public required string Name { get; set; }
+
+		/// <summary>
+		/// Navigation to CashCashMachine.
+		/// </summary>
+		public ICollection<CashCashMachine> CashCashMachines { get; set; }
+
+		/// <summary>
+		/// Department identifier.
+		/// </summary>
+		public int DepartmentId { get; set; }
+
+		/// <summary>
+		/// Navigate to Operation.
+		/// </summary>
+		public Department Department { get; set; }
+	}
+}
---
+++ src/LetusCountApplication/LetusCountApplication.Domain/Models/CashMachine.cs
@@ -0,0 +1,25 @@
+﻿namespace LetusCountApplication.Domain.Models
+{
+	public class CashMachine
+	{
+		/// <summary>
+		/// Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Serial number.
+		/// </summary>
+		public required string Serial { get; set; }
+
+		/// <summary>
+		/// Inventory number.
+		/// </summary>
+		public required int Number { get; set; }
+
+		/// <summary>
+		/// Navigation to CashCashMachine.
+		/// </summary>
+		public ICollection<CashCashMachine> CashCashMachine { get; set; }
+	}
+}
