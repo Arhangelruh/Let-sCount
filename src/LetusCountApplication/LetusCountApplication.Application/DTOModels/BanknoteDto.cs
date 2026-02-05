4a61fe8e50a8e38c03c85135e542db4227746825
Ruh <ruh@tut.by>
2026-02-05 16:28:45 +0300

    init: add dto models to work with operations

---
+++ src/LetusCountApplication/LetusCountApplication.Application/DTOModels/BanknoteDto.cs
@@ -0,0 +1,30 @@
+﻿namespace LetusCountApplication.Application.DTOModels
+{
+	public class BanknoteDto
+	{
+		/// <summary>
+		/// Banknote id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Denomination serial.
+		/// </summary>
+		public required string DenomName { get; set; }
+
+		/// <summary>
+		/// Banknote value.
+		/// </summary>
+		public int Value { get; set; }
+
+		/// <summary>
+		/// Banknote Serial number.
+		/// </summary>
+		public required string SerialNumber { get; set; }
+
+		/// <summary>
+		/// Operation unit identifer.
+		/// </summary>
+		public int OperationUnitId { get; set; }
+	}
+}
---
+++ src/LetusCountApplication/LetusCountApplication.Application/DTOModels/OperationUnitDto.cs
@@ -0,0 +1,25 @@
+﻿namespace LetusCountApplication.Application.DTOModels
+{
+	public class OperationUnitDto
+	{
+		/// <summary>
+		/// Operation unit id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Unit currency.
+		/// </summary>
+		public required string Currency { get; set; }
+
+		/// <summary>
+		/// Operation identifier.
+		/// </summary>
+		public int OperationId { get; set; }
+
+		/// <summary>
+		/// Banknotes.
+		/// </summary>
+		public required List<BanknoteDto> Banknotes { get; set; }
+	}
+}
