06311176ca4a60ad7988b2160072dee879c7670a
Ruh <ruh@tut.by>
2026-02-13 08:23:53 +0300

    init: add UI logic to configure cash machines.

---
+++ src/LetusCountApplication/LetusCountApplicationWebUI/Controllers/ConfigurationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LetusCountApplicationWebUI.Controllers
+{
+	public class ConfigurationController : Controller
+	{
+		/// <summary>
+		/// Get main configuration page.
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public IActionResult Configuration()
+		{
+			return View();
+		}
+	}
+}
--- src/LetusCountApplication/LetusCountApplicationWebUI/Program.cs
+++ src/LetusCountApplication/LetusCountApplicationWebUI/Program.cs
@@ -26,8 +26,7 @@ try
 	if (!app.Environment.IsDevelopment())
 	{
 		app.UseExceptionHandler("/Error");
-		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-		app.UseHsts();
+		app.UseStatusCodePagesWithReExecute("/Error/{0}");
 	}
 
 	app.UseHttpsRedirection();
