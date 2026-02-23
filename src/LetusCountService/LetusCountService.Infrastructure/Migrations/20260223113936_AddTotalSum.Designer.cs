3c52a811161c23db5ab1453ed7913c17ba2a7d66
Ruh <ruh@tut.by>
2026-02-23 15:26:31 +0300

    init: add migrations

--- src/LetusCountService/LetusCountService.Infrastructure/Migrations/20260223113936_AddTotalSum.Designer.cs
+++ src/LetusCountService/LetusCountService.Infrastructure/Migrations/20260223113936_AddTotalSum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LetusCountService.Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LetusCountService.Infrastructure.Migrations
 {
     [DbContext(typeof(LetusCountServiceContext))]
-    partial class LetusCountServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20260223113936_AddTotalSum")]
+    partial class AddTotalSum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/LetusCountService/LetusCountService.Infrastructure/Migrations/20260223113936_AddTotalSum.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LetusCountService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalSum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalSum",
+                schema: "opr",
+                table: "OperationUnits",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "opr",
+                table: "Banknotes",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalSum",
+                schema: "opr",
+                table: "OperationUnits");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                schema: "opr",
+                table: "Banknotes",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
